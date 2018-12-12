7c3e6812ffad1e469c4ac9f06321f672f26be7f8
srikant132 <srikantcs132@gmail.com>
2018-12-12 09:43:47 +0530

    job model created

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')  #here image model and source to save value
+    summary = models.CharField(max_length=200)
