07345bee205bc9ec0da7fa9db0c7755b63afe5d9
srikant132 <srikantcs132@gmail.com>
2018-12-13 10:02:31 +0530

    connected to new postgresql from sqlite3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER' :'srikantdb',
+        'PASSWORD':'Srk@123',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
