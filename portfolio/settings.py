1f6000032bf8821609858a0ab618ac5e0c923a4c
srikant132 <srikantcs132@gmail.com>
2018-12-14 15:25:45 +0530

    Create static files and add put data and run this cmd  python manage.py collectstatic

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,7 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_DIRS = [
+
+ os.path.join(BASE_DIR, 'portfolio/static/')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  #this used to save file in media file
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
