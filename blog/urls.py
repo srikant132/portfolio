e98b7aba6c9fbea0fc7ff574a3d6c0d9643ee775
srikant132 <srikantcs132@gmail.com>
2018-12-14 14:26:36 +0530

    these are some change that get post summary by id

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 
     path('',views.allblogs,name='allblogs'),
-
+    path('<int:blog_id>/',views.detail,name='detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Blog                                         #to get blogs data import model 1
 # Create your views here.
 def allblogs(request):
-    blogs = Blog.objects                                         #to get blog data from the database create object 2
-    return render(request,'blog/allblogs.html',{'blogs':blogs})   #create a context here blogs 3
+    blogs = Blog.objects                                          #to get blog data from the database create object 2
+    return render(request,'blog/allblogs.html',{'blogs':blogs})     #create a context here blogs 3
+
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request,'blog/detail.html',{'blog':detailblog})
