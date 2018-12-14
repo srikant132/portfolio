bdd72994c03866eb2079b6a1ae6cd283b989245f
srikant132 <srikantcs132@gmail.com>
2018-12-14 14:06:40 +0530

    blog related data fetching from db

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,6 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:100]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Blog                                         #to get blogs data import model 1
 # Create your views here.
 def allblogs(request):
-    return render(request,'blog/allblogs.html')
+    blogs = Blog.objects                                         #to get blog data from the database create object 2
+    return render(request,'blog/allblogs.html',{'blogs':blogs})   #create a context here blogs 3
