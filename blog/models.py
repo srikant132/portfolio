8a67180c3ff93bf40bcd57f32e30aca085fbef6d
srikant132 <srikantcs132@gmail.com>
2018-12-14 11:37:29 +0530

    here we set the my blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+''' # Create  ablog models
+  #title
+  #pub_date
+  #body
+  #Image
+#Add the blog app to the settng
+
+#create Migtation
+#migrate
+#Add to the admin  '''
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
